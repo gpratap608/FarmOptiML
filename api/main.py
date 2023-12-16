c9c27afd8e417b541f63ee157e98e2b7828152da
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2023-12-16 12:31:39 +0530

    New Dataset  added

--- api/main.py
+++ api/main.py
@@ -11,56 +11,56 @@ def hello_world():
     return "<h1> Hello, World </h1>"
 
 @app.route("/urea/<n>/<m>")
-def urea(n:int,m:int):
-    dataset = pd.read_csv("Nitrogen.csv")
+def urea(n:int,m:float):
+    dataset = pd.read_csv("dataset/Nitrogen.csv")
     X = dataset.iloc[:,1:-1].values
     y = dataset.iloc[:,-1].values
     from sklearn.ensemble import  RandomForestRegressor
     regressor = RandomForestRegressor(n_estimators=10,random_state=0)
     regressor.fit(X,y)
     pred=regressor.predict([[n,m]])
+    pred=float(pred)
     predstr=str(pred)
-    predstrday=str((pred/365)*1000)
     result={
         "prediction":predstr,
-        "for_a_day":predstrday
     }
     return jsonify(result)
+
 
 @app.route("/phosphate/<n>/<m>")
-def phosphate(n:int,m:int):
-    dataset = pd.read_csv("phosphate.csv")
+def phosphate(n:int,m:float):
+    dataset = pd.read_csv("dataset/phosphate.csv")
     X = dataset.iloc[:,1:-1].values
     y = dataset.iloc[:,-1].values
     from sklearn.ensemble import  RandomForestRegressor
     regressor = RandomForestRegressor(n_estimators=10,random_state=0)
     regressor.fit(X,y)
     pred = regressor.predict([[n,m]])
+    pred=float(pred)
     predstr=str(pred)
-    predstrday=str((pred/365)*1000)
     result={
         "prediction":predstr,
-        "for_a_day":predstrday
     }
     return jsonify(result)
 
+
 @app.route("/potash/<n>/<m>")
-def potash(n:int,m:int):
-    dataset = pd.read_csv("potash.csv")
+def potash(n:int,m:float):
+    dataset = pd.read_csv("dataset/potash.csv")
     X = dataset.iloc[:,1:-1].values
     y = dataset.iloc[:,-1].values
     from sklearn.ensemble import  RandomForestRegressor
     regressor = RandomForestRegressor(n_estimators=10,random_state=0)
     regressor.fit(X,y)
     pred = regressor.predict([[n,m]])
-    pred=int(pred[0])
+    pred=float(pred)
     predstr=str(pred)
-    predstrday=str((pred/365)*1000)
     result={
         "prediction":predstr,
-        "for_a_day":predstrday
     }
     return jsonify(result)
+
+
 with app.test_request_context():
     print(url_for('hello_world'))
     print(url_for('urea',n=11,m=9))
@@ -68,4 +68,4 @@ with app.test_request_context():
     print(url_for('potash',n=11,m=9))
 
 if (__name__ == "__main__"):
-    app.run(debug = True,host=0.0.0.0 , port = 8080)
+    app.run(debug = True, port = 8080, host='0.0.0.0')
